3f2994ab130181d36e2f58807d03cac156e358d0
Fahd <fahdnsheikh@gmail.com>
2015-03-16 19:31:01 -0700

    fixed file structure

--- client/app/results/results.controller.js
+++ client/app/results/results.controller.js
@@ -4,7 +4,7 @@ angular.module('snackReactorApp')
   .controller('ResultsCtrl', function ($scope, $http, $log) {
     $scope.awesomeThings = [];
 
-    // $http.get('/api/things').success(function(awesomeThings) {
-    //   $scope.awesomeThings = awesomeThings;
-    // });
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
 });
