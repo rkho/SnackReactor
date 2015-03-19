e57446818b28c364445d07a256952ac43b716992
Mike Donahue <mtdonahue1@gmail.com>
2015-03-18 21:22:29 -0700

    Completed search request, returns 3 random restaurants based on input

--- server/api/search/search.controller.js
+++ server/api/search/search.controller.js
@@ -12,6 +12,21 @@ var Restaurant = require('../../database/models/restaurant');
 exports.search = {
   // Issues a search to the DB
   query: function(req, res) {
-    
+    var price = req.body.price;
+    var health = req.body.health;
+    // var organization_id = req.user.organization_id;
+
+    new Restaurant()
+      .query({where: {price: price, health: health}}) // add organization_id here
+      .query('orderByRaw', 'random()')
+      .query('limit', '3')
+      .fetchAll().then(function(model) {
+        if (model) {
+          res.send(200, model);
+        } else {
+          res.redirect(400, '/');
+          console.log('An error has occured.');
+        }
+      });
   }
 };
