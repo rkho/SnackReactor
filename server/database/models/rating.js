965f07aaf0424000d58a3a5d4e9b363b48d2ee03
Lauren McDonnell <mcdonnell88@gmail.com>
2015-03-18 17:42:14 -0700

    notes on relationship between ratings / users

--- server/database/models/rating.js
+++ server/database/models/rating.js
@@ -8,6 +8,8 @@ var Rating = db.Model.extend({
   tableName: 'ratings',
   hasTimestamps: true,
 
+// same here -- currently set so that each rating belongs to many users, but this depends on when we calculate the average rating; seems like it would be easiest to recalculate the average every time a user rates a restaurant and then update the table with the new rating each time
+// (alternative = calculate the average from all ratings in the table each time we display a restaurant; would be  expensive / inefficient to do it this way)
   users: function(){
     return this.belongsToMany(User, 'user_id');
   },
