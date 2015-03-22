41cd40e76efa471a68f33068774ef27729b4e808
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-03-21 22:44:52 -0700

    Complete add restaurant page design.

--- client/app/create_restaurant/create.restaurant.controller.js
+++ client/app/create_restaurant/create.restaurant.controller.js
@@ -29,10 +29,35 @@ app.controller('CreateRestCtrl', function ($scope, $modal, $log, CheckLoggedIn, 
 
 app.controller('4Ctrl', function ($scope, $modalInstance, items, OrgSelect, $location) {
 
-  $scope.hearts=[{num: 0, clicked:false, descrition: 'Junk food!'}, {num:1, clicked: false}, {num:2, clicked: false}];
+  $scope.heartText = '';
+  $scope.submitting = false;
+
+  $scope.hoverHeart = function(value) {
+    var heartText = {
+      1: 'Junk food!',
+      2: 'Any food will do.',
+      3: 'Something healthy, please!',
+      4: ''
+    };
+    $scope.heartText = heartText[value];
+  };
+
+  $scope.priceText = '';
+
+  $scope.hoverPrice = function(value) {
+    var priceText = {
+      1: '$7 & Under',
+      2: '$8 - $15',
+      3: '$15 ++',
+      4: ''
+    };
+    $scope.priceText = priceText[value];
+  };
+
+
+  $scope.isCollapsed = false;
 
   var search = $location.search();
-  $scope.submitting = false;
  
   $scope.ok = function () {
     $modalInstance.close($scope.selected.item);
