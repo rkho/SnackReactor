5a3b7845f49d0dab9b72004abbbab6d76c0ad381
Bradley Portnoy via Circle-CI <bradley.portnoy@gmail.com>
2015-03-23 11:51:11 -0700

    Built snack-reactor from commit 9ae7134 on branch master

--- server/api/search/search.controller.js
+++ server/api/search/search.controller.js
@@ -16,15 +16,19 @@ exports.search = {
     var price = req.body.price;
     var health = req.body.health;
     // var organization_id = req.user.organization_id;
-
     new Restaurant()
       .query(function(qb){
         qb.where('health', health).andWhere('price', '>=', price).orderByRaw('random()').limit(3);// add organization_id here
       }) 
       .fetchAll().then(function(models) {
+        console.log(models);
         if (models.size()) {
           var results = [];
           models.forEach(function(model){
+            //transform the photo reference into a URL
+            model.set('photo_url',
+              'https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&photoreference=' + model.get('photo_url') + '&key=AIzaSyDUYAAHTfuH1FhBacOWtF01FZGjF7Sd3mc');
+            //and push the result into the results array
             results.push(model.toJSON());
           });
           res.send(200, results);
