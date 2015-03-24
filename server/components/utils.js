aaaade5f9e706e90faec31e46e446e28522b1027
Bradley Portnoy via Circle-CI <bradley.portnoy@gmail.com>
2015-03-23 17:28:35 -0700

    Built snack-reactor from commit c2a93c0 on branch master

--- server/components/utils.js
+++ server/components/utils.js
@@ -1,5 +1,35 @@
+var OrganizationRestaurant = require('../database/models/organization_restaurant.js');
+var Rating = require('../database/models/rating.js');
+
 exports.authenticate = function(req, res, next){
-  console.log(req.user);
   if (!req.isAuthenticated()) res.send(401);
   else next();
+};
+
+exports.calculateAvgRating = function(restaurantId, orgId){
+  console.log('rest, org ' + restaurantId + ' ' + orgId);
+  Rating.forge()
+  .query(function(qb){
+    qb.where({restaurant_id: restaurantId, organization_id: orgId})
+    .avg('rating');
+  }).fetch()
+  .then(function(rating){
+    var avgRating = rating.get('avg("rating")');
+    OrganizationRestaurant.forge({restaurant_id: restaurantId, organization_id: orgId})
+    .fetch()
+    .then(function(orgRest){
+      if (orgRest){
+        orgRest.set('avg_rating', avgRating);
+        orgRest.save();
+      } else {
+        OrganizationRestaurant.forge({
+          organization_id: orgId,
+          restaurant_id: restaurantId,
+          avg_rating: avgRating
+        })
+        .save();
+      }
+    });
+  });
+
 };
