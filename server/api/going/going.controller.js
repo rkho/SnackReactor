7099898e74a30f9d79534a0e6eef4cd5000deb21
scottdixon <scott@howrefreshing.com.au>
2015-03-25 11:40:38 -0700

    New API endpoint: /going/create

--- server/api/going/going.controller.js
+++ server/api/going/going.controller.js
@@ -1,25 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-// var request = require('superagent');
-// var authenticate = require('../../components/utils.js').authenticate;
-// var Restaurant = require('../../database/models/restaurant.js');
-// var User = require('../../database/models/user.js');
-// var Going = require('../../database/models/going.js');
-
-// router.post('/going/create', authenticate, function(req,res){
-
-// 	new Going({
-// 	  user_id: req.user.id,
-// 	  restaurant_id: req.body.id,
-// 	}).save()
-// 	.then(function(){
-// 	  res.send(201, 'Created');
-// 	});
-
-// })
-
-// module.exports = router;
-
 /**
  * Endpoints
  * POST    /going/create     ->  create
