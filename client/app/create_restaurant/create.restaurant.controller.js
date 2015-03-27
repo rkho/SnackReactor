dd0b86ba4722f862826d592755294e97b6985dd1
Richard Kho <rkho@me.com>
2015-03-27 09:59:52 -0700

    Cleaned up code

--- client/app/create_restaurant/create.restaurant.controller.js
+++ client/app/create_restaurant/create.restaurant.controller.js
@@ -10,14 +10,6 @@ app.controller('CreateRestCtrl', function ($scope, $window, $http, $location, Cr
 
   var search = $location.search();
 
-  // $scope.ok = function () {
-  //   $modalInstance.close($scope.selected.item);
-  // };
-
-  // $scope.cancel = function () {
-  //   $modalInstance.dismiss('cancel');
-  // };
-
   $scope.successMessage = '';
   $scope.failureMessage = '';
 
@@ -27,24 +19,6 @@ app.controller('CreateRestCtrl', function ($scope, $window, $http, $location, Cr
         console.log('Hooray!');
       });
   }
-
-  // $scope.submitRestaurant = function(){
-  //   $scope.submitting = true;
-  //   $scope.successMessage = '';
-  //   $scope.failureMessage = '';
-  //   console.log();
-  //   CreateRestaurant($scope.createRest.name, $scope.createRest.address, $scope.createRest.healthRating, $scope.createRest.priceRating, $scope.createRest.description, $scope.createRest.rating)
-  //   .success(function(data, status, headers, config){
-  //     $scope.submitting = false;
-  //     $scope.successMessage = 'Restaurant created successfully, thanks!';
-  //     $window.location.reload();
-  //   })
-  //   .error(function(data,status,headers,config){
-  //     $scope.submitting = false;
-  //     $scope.failureMessage = 'Error creating restaurant. Are you sure you have the correct address?';
-  //     console.error('Error creating restaurant: ' + data);
-  //   });
-  // };
 
   $scope.cancelButton = function() {
     $window.location.href = '/';
