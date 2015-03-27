e494033c6f96f705d7e7ed06a5f639a54ad4c276
Richard Kho <rkho@me.com>
2015-03-26 17:01:58 -0700

    Hooked in function to add an entry to database

--- client/app/create_restaurant/create.restaurant.controller.js
+++ client/app/create_restaurant/create.restaurant.controller.js
@@ -1,33 +1,8 @@
 'use strict';
 
 var app = angular.module('snackReactorApp');
-//refactor to services
 
-// app.controller('CreateRestCtrl', function ($scope, $modal, $log, CheckLoggedIn, ModalService) {
-//   // $scope.items = [];
-
-//   $scope.open = function (size) {
-//     var modalInstance = $modal.open({
-//       templateUrl: '4.html',
-//       controller: '4Ctrl',
-//       size: size,
-//       // backdrop: 'static',
-//       resolve: {
-//         items: function () {
-//           return $scope.items;
-//         }
-//       }
-//     });
-//     modalInstance.result.then(function (selectedItem) {
-//       $scope.selected = selectedItem;
-//     });
-//   };
-
-//   $scope.open('md');
-
-// });
-
-app.controller('CreateRestCtrl', function ($scope, $window, $http, OrgSelect, $location, CreateRestaurant) {
+app.controller('CreateRestCtrl', function ($scope, $window, $http, $location, CreateRestaurant) {
 
   $scope.submitting = false;
 
@@ -46,23 +21,27 @@ app.controller('CreateRestCtrl', function ($scope, $window, $http, OrgSelect, $l
   $scope.successMessage = '';
   $scope.failureMessage = '';
 
-  $scope.submitRestaurant = function(){
-    $scope.submitting = true;
-    $scope.successMessage = '';
-    $scope.failureMessage = '';
-    console.log();
-    CreateRestaurant($scope.createRest.name, $scope.createRest.address, $scope.createRest.healthRating, $scope.createRest.priceRating, $scope.createRest.description, $scope.createRest.rating)
-    .success(function(data, status, headers, config){
-      $scope.submitting = false;
-      $scope.successMessage = 'Restaurant created successfully, thanks!';
-      $window.location.reload();
-    })
-    .error(function(data,status,headers,config){
-      $scope.submitting = false;
-      $scope.failureMessage = 'Error creating restaurant. Are you sure you have the correct address?';
-      console.error('Error creating restaurant: ' + data);
-    });
-  };
+  $scope.addRestaurant = function(object){
+    CreateRestaurant(object.name, object.formatted_address, object.formatted_phone_number, object.place_id);
+  }
+
+  // $scope.submitRestaurant = function(){
+  //   $scope.submitting = true;
+  //   $scope.successMessage = '';
+  //   $scope.failureMessage = '';
+  //   console.log();
+  //   CreateRestaurant($scope.createRest.name, $scope.createRest.address, $scope.createRest.healthRating, $scope.createRest.priceRating, $scope.createRest.description, $scope.createRest.rating)
+  //   .success(function(data, status, headers, config){
+  //     $scope.submitting = false;
+  //     $scope.successMessage = 'Restaurant created successfully, thanks!';
+  //     $window.location.reload();
+  //   })
+  //   .error(function(data,status,headers,config){
+  //     $scope.submitting = false;
+  //     $scope.failureMessage = 'Error creating restaurant. Are you sure you have the correct address?';
+  //     console.error('Error creating restaurant: ' + data);
+  //   });
+  // };
 
   $scope.cancelButton = function() {
     $window.location.href = '/';
